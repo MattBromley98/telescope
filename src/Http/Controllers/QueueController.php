d91abc603352561f4e0e3af6d1da417e4799698f
Matthew Bromley <96053035+MattBromley98@users.noreply.github.com>
2024-09-30 11:59:18 +0100

    fix: Ensure that the redirect from the QueueController is successful

--- src/Http/Controllers/QueueController.php
+++ src/Http/Controllers/QueueController.php
@@ -45,9 +45,9 @@ class QueueController extends EntryController
     public function retry(Request $request)
     {
         $class = $request->get('jobType');
-        $class::dispatch($request->get('payload'));
+        $class::dispatch($request->get('payload')['data']);
 
-        return redirect()->to(route('/telescope/jobs'))->with('success', 'Successfully redeployed job');
+        return redirect()->to('/telescope/jobs')->with('success', 'Successfully redeployed job');
     }
 
     /**
