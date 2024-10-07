6f11f8a1560a42d9a3acc472328dbee00c419277
Matthew Bromley <96053035+MattBromley98@users.noreply.github.com>
2024-10-07 12:36:08 +0100

    fix: Finalise styling changes

--- resources/js/components/IndexScreen.vue
+++ resources/js/components/IndexScreen.vue
@@ -275,7 +275,7 @@
             <h2 class="h6 m-0">{{this.title}}</h2>
 
             <div class="w-75 status-area">
-                <div class="form-control-with-icon w-25" v-if="!hideSearch && (tag || entries.length > 0)">
+                <div class="form-control-with-icon w-50" v-if="!hideSearch && (tag || entries.length > 0)">
                     <div class="icon-wrapper">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon">
                             <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
@@ -285,7 +285,7 @@
                            id="searchInput"
                            placeholder="Search Tag" v-model="tag" @input.stop="search">
                 </div>
-                <div v-if="!hideSearch" class="w-75" @input.stop="search">
+                <div v-if="!hideSearch" class="w-50" @input.stop="search">
                     <select v-model="status" class="form-control status-select w-100">
                         <option value="">Select Status:</option>
                         <option value="pending">Pending</option>
